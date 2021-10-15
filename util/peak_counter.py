d12878fab3427ed72ae2ece46c5457d7c40c617e
Ruben Rodriguez Buchillon <cocontruben@cs.stanford.edu>
2021-10-14 18:58:15 -0700

    peak counting v2: with convolutions

--- util/peak_counter.py
+++ util/peak_counter.py
@@ -1,37 +1,35 @@
 import numpy as np
 
-def count_peaks(spectogram, window_size=3, floor=30.):
+def count_peaks(spectogram, window_size=3, floor=30., delta=0.):
     """Count the peaks and their start point in |spectogram|.
 
     Args:
         spectogram: 2d array of (mel bins, time dim)
         window_size: how many samples backwards and and forward to look
         floor: intensity under which to "ignore" peaks
+        delta: how large the delta between windows should be
 
     Returns:
         tuple: number of peaks, list of the center index
     """ 
-    # This works by looking at the summed intensity and finding
-    # peaks, defined as: the last |window_size| samples monotonically increase
-    # and the next |window_size| samples monotonically decrease
-    total_intensities = np.sum(np.exp(spectogram), axis=0)
+    m = np.ones(window_size)
+    total_intensities = np.sum(np.exp(spectogram - np.min(spectogram)), axis=0)
+    # total_intensities = np.sum(spectogram, axis=0)
+    # try out percentage based flooring
+    floor = np.max(total_intensities) * 0.50
     # Floor out the poor ones.
-    total_intensities[total_intensities < floor] = 0.
+    # total_intensities[total_intensities < floor] = 0.
     # Let's make an array of whether a[i] > a[i-1]
-    decreasing = np.zeros(shape=total_intensities.shape)
-    for i in np.arange(1, len(total_intensities)):
-        if total_intensities[i-1] > total_intensities[i]:
-            decreasing[i-1] = 1
-    # Now, let's sum all the numbers up to when we see a 0.
-    # TODO(coconutruben): this does only the forward pass
-    # we might want to implement the backwards pass (monotonically decreasing)
-    # as well
-    up_to = np.zeros(shape=total_intensities.shape)
-    for i in np.arange(len(total_intensities)-1, 0, -1):
-        if decreasing[i] == 1:
-            up_to[i] += 1 + up_to[i+1]
-            up_to[i+1] = 0
-    # Now, we can simply look for the window-size numbers
-    indexes = np.argwhere(up_to > window_size)
+    window_intensities = np.convolve(total_intensities, m, mode='valid')
+    # floor = np.max(window_intensities) * 0.50
+    indexes = []
+    for idx in range(1, len(window_intensities)-1):
+        if window_intensities[idx-1] + delta < window_intensities[idx]:
+            if window_intensities[idx+1] + delta < window_intensities[idx]:
+                # The offset is necessary since that's where the original
+                # conv window was centered.
+                ridx = idx + window_size // 2 - 1
+                if total_intensities[ridx] > floor:
+                    indexes.append(ridx)
     count = len(indexes)
-    return count, indexes.ravel()
+    return count, indexes
